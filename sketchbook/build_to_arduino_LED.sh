cab35c4015b71a54f73a5edda85639883502b266
Nicholas Liu <nicholas.j.liu@gmail.com>
2017-11-09 22:47:58 -0800

    Cleaned Arduino testing scripts

--- sketchbook/build_to_arduino_LED.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash ~/arduino-1.8.4/arduino --upload /home/ubuntu/sketchbook/arduino_node_LED/arduino_node_LED.ino --port /dev/ttyACM0 --board arduino:avr:uno
--- sketchbook/serial_build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash ~/arduino-1.8.4/arduino --upload /home/ubuntu/sketchbook/serial_test/serial_test.ino --port /dev/ttyACM0 --board arduino:avr:uno
