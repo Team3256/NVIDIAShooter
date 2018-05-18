439e870048a526224767db75ef1132fdc849fc61
apache8080 <ezhang887@gmail.com>
2018-05-17 17:22:56 -0700

    update for spring 2018

--- shooter_ws/src/shooter/scripts/jetson_node.py
+++ shooter_ws/src/shooter/scripts/jetson_node.py
@@ -48,9 +48,11 @@ def callback(data):
 	array = Float32MultiArray()
 	
 	#Arcade Drive
-	ldrive,rdrive = arcadeDrive(float(data.axes[1]),float(-data.axes[2]))
+	#Mapped for Logitech Controller; not necessarily the SHIELD keys
+	ldrive,rdrive = arcadeDrive(float(data.axes[1]),float(-data.axes[3]))
 
 	#set values for the arduino from joystick input
+	''' SHIELD bindings
 	lshoot = 1 if float(data.axes[8]) > 0.5 else 0
 	rshoot = 1 if float(data.axes[9]) > 0.5 else 0
 	lshoot = lshoot and not old_lshoot
@@ -65,7 +67,26 @@ def callback(data):
 	rreload = rreload and not old_rreload
 
 	pivotForward = int(data.buttons[12])
-	pivotBackward = int(data.buttons[14])	
+	pivotBackward = int(data.buttons[14])
+	'''
+
+
+	#for logitech controller
+	lshoot = 1 if float(data.axes[2]) < 0.0 else 0
+	rshoot = 1 if float(data.axes[5]) < 0.0 else 0
+	lshoot = lshoot and not old_lshoot
+	rshoot = rshoot and not old_rshoot
+	
+	lbumper = int(data.buttons[4])
+	rbumper = int(data.buttons[5])
+
+	lreload = int(data.buttons[2])
+	lreload = lreload and not old_lreload
+	rreload = int(data.buttons[1])
+	rreload = rreload and not old_rreload
+
+	pivotForward = int(data.buttons[3])
+	pivotBackward = int(data.buttons[0])
 
 	#populate and publish the array (note: it's limited to 10 values. If more are needed, make another topic or use custom messages)
 	array.data = [ldrive,rdrive,lshoot,rshoot,lreload,rreload,pivotForward,pivotBackward, lbumper, rbumper]
